3b26572f1d12ffca484c910dd6e91cf920e7c7bc
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-18 21:53:09 +0000

    comments

--- src/js/scrollables.js
+++ src/js/scrollables.js
@@ -1,19 +1,27 @@
+//sets scrolled class on scrollables when they reach the right height
 function reveal() {
+	//get all scrollables
   var reveals = document.querySelectorAll(".scrollable");
 
 	//console.log("scrolled");
+	//set scrolled on each scrollable
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
+		//the height the element needs to reach
     var elementVisible = 300;
 
+		//if it has reached the height...
     if (elementTop < windowHeight - elementVisible) {
+			//...have it be revealed...
       reveals[i].classList.add("scrolled");
 			//console.log(reveals[i].classList);
+			//...otherwise, remove scrolled so it hides again
     } else {
       reveals[i].classList.remove("scrolled");
     }
   }
 }
 
+//event listener
 window.addEventListener("scroll", reveal);
