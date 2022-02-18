686da14a6db72c6d5d3bdc25d2422273bce61d2f
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-18 20:29:05 +0000

    change from active to scrolled

--- src/js/scrollables.js
+++ src/js/scrollables.js
@@ -1,17 +1,17 @@
 function reveal() {
   var reveals = document.querySelectorAll(".scrollable");
 
-	console.log("scrolled");
+	//console.log("scrolled");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
     var elementVisible = 500;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-			console.log(reveals[i].classList);
+      reveals[i].classList.add("scrolled");
+			//console.log(reveals[i].classList);
     } else {
-      reveals[i].classList.remove("active");
+      reveals[i].classList.remove("scrolled");
     }
   }
 }
